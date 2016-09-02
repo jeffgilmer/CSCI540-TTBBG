2d84ccec5092a8c8625159172e1502cdf783ab94
0CThompson <cvt@email.uscupstate.edu>
2016-09-02 10:45:01 -0400

    I created a web app in visual studio for us to start working on

---
+++ TTBBG/TTBBG/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TTBBG.Startup))]
+namespace TTBBG
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
